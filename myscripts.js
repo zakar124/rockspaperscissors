4d44c9d3935b069a2eeff162ac566d780a59e384
Jill Zakarian <JillianZakarian@gmail.com>
2022-07-30 18:26:57 -0400

    added CSS file

--- myscripts.js
+++ myscripts.js
@@ -2,3 +2,17 @@ let choices = ["Rocks", "Paper", "Scissors"];
 
 let getComputerChoice = choices[Math.floor(Math.random()*choices.length)];
 console.log(getComputerChoice);
+
+let playerSelection = window.prompt ("Type your Selection: rock, paper or scissors").toUpperCase();
+ let computerSelection = choices[Math.floor(Math.random()*choices.length)];
+console.log(getComputerChoice);
+
+
+
+if (playerSelection == "rocks" && computerSelection == "rocks") {
+console.log("Tie!");
+}
+
+if (playerSelection == "rocks" && computerSelection == "scissors") {
+    console.log("You Win!");
+}
