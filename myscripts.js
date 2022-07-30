f1340dcc2ef91b1dae5ed3183350e3ae3ecd14e8
Jill Zakarian <JillianZakarian@gmail.com>
2022-07-29 21:57:14 -0400

    added Math.random function

--- myscripts.js
+++ myscripts.js
@@ -1,1 +1,4 @@
-console.log("hello there");
+let choices = ["Rocks", "Paper", "Scissors"];
+
+let getComputerChoice = choices[Math.floor(Math.random()*choices.length)];
+console.log(getComputerChoice);
