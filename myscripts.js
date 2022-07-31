781e0ddc18b7cafda62b73404ea29227dcd46c0b
Jill Zakarian <JillianZakarian@gmail.com>
2022-07-31 16:31:07 -0400

    made loop

--- myscripts.js
+++ myscripts.js
@@ -1,18 +1,35 @@
-let choices = ["Rocks", "Paper", "Scissors"];
 
-let getComputerChoice = choices[Math.floor(Math.random()*choices.length)];
-console.log(getComputerChoice);
+const choices = ["Rocks", "Paper", "Scissors"]
+function getComputerChoice() {
+    return choices[Math.floor(Math.random() * Math.floor(choices.length))]
+}
+let playerSelection = prompt("Please choose Rocks, Paper or Scissors");
+alert("HiiiYOOOO, you chose " + playerSelection.toUpperCase()+"!" + "\nMay the odds ever be in your favor!");
 
-let playerSelection = window.prompt ("Type your Selection: rock, paper or scissors").toUpperCase();
- let computerSelection = choices[Math.floor(Math.random()*choices.length)];
-console.log(getComputerChoice);
+const computerSelection = getComputerChoice();
 
 
-function 
-if (playerSelection == "rocks" && computerSelection == "rocks") {
-return "Tie!";
+function playRound(playerSelection, computerSelection) {
+
+    if ((playerSelection === 'Rocks' && computerSelection === 'Scissors' || playerSelection === 'Paper' && computerSelection === 'Rocks' || playerSelection === 'Scissors' && computerSelection === 'Paper')) {
+        return "YOU WIN!";
+    }
+
+    else if ((playerSelection === 'Rocks' && computerSelection === 'Paper' || playerSelection === 'Paper' && computerSelection === 'Scissors' || playerSelection === 'Scissors' && computerSelection === 'Rocks')) {
+        return "YOU LOSE!";
+    }
+
+    else if (playerSelection === computerSelection) {
+        return "TIE!";
+    }
 }
 
-if (playerSelection == "rocks" && computerSelection == "scissors") {
-    return "You Win!";
+console.log("You chose " + playerSelection.toUpperCase());
+console.log("Computer chose " + computerSelection);
+console.log(playRound(playerSelection, computerSelection));
+
+for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Please choose Rocks, Paper or Scissors");
+alert("HiiiYOOOO, you chose " + playerSelection.toUpperCase()+"!" + "\nMay the odds ever be in your favor!");
+    console.log(playRound(playerSelection, computerSelection));
 }
