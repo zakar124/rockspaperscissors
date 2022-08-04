503bf609f86724ec20941c7fb0ab44c8128fc343
Jill Zakarian <JillianZakarian@gmail.com>
2022-08-04 15:44:31 -0400

    added pop-up function to javascript; added CSS

--- myscripts.js
+++ myscripts.js
@@ -3,21 +3,36 @@ const choices =["rock", "paper", "scissors"];
 computerSelection = choices[Math.floor(Math.random() * choices.length)];
 
 
-playerSelection = prompt("Let's Play 5 Rounds...\n\nMan Vs. Computer!\n\nType ROCK, PAPER or SCISSORS");
+function rockspopUp() {
+  let popupRocks = document.getElementById("Rocks");
+  popupRocks.classList.toggle("show");
+  } 
 
+  function paperpopUp() {
+  let popupPaper = document.getElementById("Paper");
+  popupPaper.classList.toggle("show");
+  }
+  function scissorspopUp () {
+  let popupScissors = document.getElementById("Scissors");
+  popupScissors.classList.toggle("show");
+
+}
 
 function playRound (playerSelection, computerSelection) {
 
-  if ((playerSelection === "rock" && computerSelection === "paper" ) || (playerSelection === "paper" && computerSelection === "scissors" )) {
-    alert ("Sorry. You Lose.");
+  if ((playerSelection === "rock" && computerSelection === "paper" ) 
+  || (playerSelection === "paper" && computerSelection === "scissors" )) {
+   alert ("Sorry. You Lose.");
   }
   
-  if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" & computerSelection == "paper")) {
-    alert ("Hooray! You Win!");
+  if ((playerSelection === "rock" && computerSelection === "scissors") 
+  || (playerSelection === "paper" && computerSelection === "rock") 
+  || (playerSelection === "scissors" & computerSelection == "paper")) {
+    return "Hooray! You Win!";
   }
 
   if (playerSelection === computerSelection) {
-    alert ("Tie! Nice work!");
+    return "Tie! Nice try.";
   }
 }
 
