a9f35fd29b064d93f0a7931d7678514427477c73
Jill Zakarian <JillianZakarian@gmail.com>
2022-09-03 18:31:14 -0400

    updated css , html & javascript

--- myscripts.js
+++ myscripts.js
@@ -1,50 +1,92 @@
-const choices =["rock", "paper", "scissors"];
-
-computerSelection = choices[Math.floor(Math.random() * choices.length)];
 
 
-function rockspopUp() {
-  let popupRocks = document.getElementById("Rocks");
-  popupRocks.classList.toggle("show");
-  } 
 
-  function paperpopUp() {
-  let popupPaper = document.getElementById("Paper");
-  popupPaper.classList.toggle("show");
+const choices = document.querySelectorAll(".choices");
+
+let yourScore = document.querySelector(".yourScore");
+let computerScore = document.querySelector(".computerScore");
+let imageBorder = document.querySelector(".imageBorder");
+let winner = document.querySelector(".winner");
+let youChose = document.querySelector(".youChose");
+let computerChose = document.querySelector(".computerChose");
+let round = document.querySelector(".round");
+
+yourScore.textContent = 0;
+computerScore.textContent = 0;
+round.textContent = 0;
+
+
+// choices.classList.add("imageBorder");
+let playerInput = choices.value;
+
+const computerChoices = ["Rock", "Paper", "Scissors"];
+let computerInput = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", function () {
+    const playerInput = this.value;
+    let computerInput = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    round.textContent++
+
+  if ((playerInput === "Rock" && computerInput === "Paper" ) 
+  || (playerInput === "Paper" && computerInput === "Scissors" )) {
+    winner.textContent = `YOU LOSE Round ${round.textContent}`;
+    youChose.textContent = `${playerInput}`;
+    computerChose.textContent = `${computerInput}`;
+    computerScore.textContent++;
+    
   }
+
+ 
+  else if ((playerInput === "Rock" && computerInput === "Scissors") 
+  || (playerInput === "Paper" && computerInput === "Rock") 
+  || (playerInput === "Scissors" & computerInput == "Paper")) {
+    winner.textContent = `YOU WIN Round ${round.textContent}`;
+    youChose.textContent = `${playerInput}`;
+    computerChose.textContent = `${computerInput}`;
+    yourScore.textContent++;
+  }
+
+  else if (playerInput === computerInput) {
+    winner.textContent = `TIED for Round ${round.textContent}`;
+    youChose.textContent = `${playerInput}`;
+    computerChose.textContent = `${computerInput}`;;
+  }
+
+  if ((round.textContent == 5) && (computerScore.textContent > yourScore.textContent)) {
+    winner.textContent = `Final Round: Bully Wins All
+    😩😒`;
+    yourScore.textContent = 0;
+  computerScore.textContent =0;
   
-  function scissorspopUp () {
-  let popupScissors = document.getElementById("Scissors");
-  popupScissors.classList.toggle("show");
-
-}
-
-function playRound (playerSelection, computerSelection) {
-
-  if ((playerSelection === "rock" && computerSelection === "paper" ) 
-  || (playerSelection === "paper" && computerSelection === "scissors" )) {
-   alert ("Sorry. You Lose.");
   }
+    else if ((round.textContent == 5) && (yourScore.textContent > computerScore.textContent)) {
+      winner.textContent =`Final Round: You Win 
+      All!   
+      🎉 🏆 🎉 🏆  🎉 `;
+      yourScore.textContent = 0;
+      computerScore.textContent =0;
+
+  } else if ((round.textContent == 5) && (yourScore.textContent == computerScore.textContent)) {
+    winner.textContent =`Final Round: Tied 🙃`;
+    yourScore.textContent = 0;
+    computerScore.textContent =0;
+     
+  }
+  if (round.textContent == 5) {
+    round.textContent = 0;
+  }
+  });
+});
+document.querySelector('.button').addEventListener('click', function () {
+  round.textContent = 0;
+  yourScore.textContent = 0;
+  computerScore.textContent =0;
+  winner.textContent = "";
+  youChose.textContent = "";
+  computerChose.textContent = "";
+
   
-  if ((playerSelection === "rock" && computerSelection === "scissors") 
-  || (playerSelection === "paper" && computerSelection === "rock") 
-  || (playerSelection === "scissors" & computerSelection == "paper")) {
-    return "Hooray! You Win!";
-  }
+});
 
-  if (playerSelection === computerSelection) {
-    return "Tie! Nice try.";
-  }
-}
-
-console.log("You chose " + playerSelection);
-console.log("Computer chose " + computerSelection);
-console.log(playRound(playerSelection.toLowerCase(), computerSelection));
-
-for (let i = 0; i < 4; i++) {
-  playerSelection = prompt("Let's Play 5 Rounds...\n\nMan Vs. Computer!\n\nType ROCK, PAPER or SCISSORS");
-  console.log("You chose " + playerSelection);
-  console.log("Computer chose " + computerSelection);
-  console.log(playRound(playerSelection.toLowerCase(), computerSelection));
-
-}
